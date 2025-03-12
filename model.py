230661d87a416374be39543a03065fffab021864
yu jie Yao <103044520+swicher123@users.noreply.github.com>
2025-03-12 15:52:18 +0800

    Update model.py

--- model.py
+++ model.py
@@ -11,17 +11,19 @@ from tensorflow.keras.models import Model
 
 
 
-def CNN(input_shape, num_labels, length):
-    main_input = Input(shape=(input_shape,), dtype='int64', name='main_input')
-    x = Embedding(output_dim=128, input_dim=200, input_length=length, name='Embadding')(main_input)
+def CNN(input_shape, num_labels):
+    main_input = Input(shape=(input_shape,), dtype='float32', name='main_input')
 
-    a = Conv1D(64, 2, activation='relu', padding='same')(x)
-    apool = MaxPooling1D(pool_size=2, strides=1, padding='same')(a)
-    merge = Dropout(0.6)(apool)
+    x = main_input[..., None]
 
-    x = Flatten()(merge)
+    # 1D 卷积可以保留，但要调整参数
+    x = Conv1D(64, kernel_size=2, activation='relu', padding='same')(x)
+    x = MaxPooling1D(pool_size=2, strides=1, padding='same')(x)
+
+    x = Flatten()(x)
     x = Dense(128, activation='relu')(x)
+    x = Dropout(0.4)(x)  # Dropout 位置调整
     output = Dense(num_labels, activation='sigmoid', name='output')(x)
-    model = Model(inputs=main_input, outputs=output)
 
+    model = Model(inputs=main_input, outputs=output)
     return model
